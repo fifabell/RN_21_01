e64ad90df6c50cfef0cee2d70f73df03dc2238ac
Jeong jong in <33538410+fifabell@users.noreply.github.com>
2021-03-02 16:16:32 +0900

    [210302]

--- App.js
+++ App.js
@@ -1,51 +1,47 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { WebView } from 'react-native-webview'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-const domain = 'https://mwebserver.parkland.co.kr/ShopApp/codeinfo_t/test2.php'
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            // indexPage: { uri: domain + '?CODE=ACL20102' }
-            indexPage: { uri: domain  },
-        };
-    }
-    
-    render() {
-        return (
-            <View style={styles.container}>
-                <WebView
-                    style={styles.webview}
-                    source={this.state.indexPage}
-                    source={{
-                      uri:   domain,  
-                      body:  'param1=1&foo=2',
-                      method:'POST'
-                    }}
-                    // source={{ uri: domain, method: 'POST', body: 'cat=himalayan&dog=pug&fish=shark'} }
-                    onMessage={(event) =>alert(event.nativeEvent.data)}
-                    originWhitelist={['*']}
-                    domStorageEnabled={true}
-                    allowFileAccessFromFileURLs={true}
-                    startInLoadingState={true}
-                    ref={webview => this.appWebview = webview}
-                    mixedContentMode="compatibility"
-                    javaScriptEnabled={true}
-                    useWebKit={true}
-                    // scalesPageToFit={false}
-                />
-            </View>
-        );
-    }
-};
+class gitbookTest extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 30
-    },
-    webview: {
-        flex: 1
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
+
+AppRegistry.registerComponent('gitbookTest', () => gitbookTest);
